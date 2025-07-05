3275f9cca02ad4264034039320dea85e2e2e78e6
JuanD <dosquisd123@gmail.com>
2025-07-05 17:35:24 -0500

    feat: implement ddl-generator server

--- excel-parsing/excel-reader/src/main.py
+++ excel-parsing/excel-reader/src/main.py
@@ -1,28 +1,46 @@
 import grpc
-from clients.formula_parser import utils
+from clients.dtypes import utils
 from services.get_data import get_data_from_spreadsheet
+from clients.dtypes import dtypes_pb2
+from clients.ddl_generator import ddl_generator_pb2, ddl_generator_pb2_grpc
 from clients.formula_parser import formula_parser_pb2, formula_parser_pb2_grpc
 
 from services import dtypes
-from typing import Generator
+from typing import Generator, Dict
 from core.config import settings
 
 
 FORMULA_PARSER_CHANNEL = grpc.insecure_channel(settings.FORMULA_PARSER_CHANNEL)
 FORMULA_PARSER_STUB = formula_parser_pb2_grpc.FormulaParserStub(FORMULA_PARSER_CHANNEL)
 
+DDL_GENERATOR_CHANNEL = grpc.insecure_channel(settings.DDL_GENERATOR_CHANNEL)
+DDL_GENERATOR_STUB = ddl_generator_pb2_grpc.DDLGeneratorStub(DDL_GENERATOR_CHANNEL)
+
 
 def parse_formula(
     stub: formula_parser_pb2_grpc.FormulaParserStub, formula: str
-) -> dict:
+) -> Dict:
     request = formula_parser_pb2.FormulaParserRequest(formula=formula)
     response: formula_parser_pb2.FormulaParserResponse = stub.ParseFormula(request)
     return response.ast
 
 
+def generate_ddl(
+    stub: ddl_generator_pb2_grpc.DDLGeneratorStub,
+    ast: dtypes_pb2.AST,
+    columns: Dict[str, str],
+) -> str:
+    print(ast)
+    print(columns)
+    request = ddl_generator_pb2.DDLRequest(ast=ast)
+    request.columns.update(columns)
+    response: ddl_generator_pb2.DDLResponse = stub.GenerateDDL(request)
+    return response.sql
+
+
 def generate_data(
-    data: dict[str, dict[str, list[dtypes.CellData]]],
-) -> Generator[dict[str, str | dtypes.CellData], None, None]:
+    data: dtypes.DataInfo,
+) -> Generator[Dict[str, str | dtypes.CellData], None, None]:
     for sheet, cols in data.items():
         for col, cells in cols.items():
             for i, cell in enumerate(cells[:1]):
@@ -34,7 +52,9 @@ def generate_data(
                 }
 
 
-def parse_formulas(filename: str, file_bytes: bytes):
+def parse_formulas(
+    filename: str, file_bytes: bytes
+) -> Dict[str, dtypes.DataInfo | dtypes.ColumnsInfo]:
     content = get_data_from_spreadsheet(filename, file_bytes)
     data = content["data"]
     result = {}
@@ -53,14 +73,19 @@ def parse_formulas(filename: str, file_bytes: bytes):
         cell["ast"] = parse_formula(FORMULA_PARSER_STUB, str(cell["value"]))
         result[sheet][col].append(cell)
 
-    return result
+    return {"result": result, "columns": content["columns"]}
 
 
-def main(filename: str, file_bytes: bytes) -> None:
-    result = parse_formulas(filename, file_bytes)
+def main(filename: str, file_bytes: bytes) -> dtypes.DataInfo:
+    content = parse_formulas(filename, file_bytes)
+    result = content["result"]
+    columns = content["columns"]
     for sheet, cols in result.items():
         for col, cells in cols.items():
             for i, cell in enumerate(cells):
+                result[sheet][col][i]["sql"] = generate_ddl(
+                    DDL_GENERATOR_STUB, cell["ast"], columns[sheet]
+                )
                 result[sheet][col][i]["ast"] = utils.parse_ast(cell["ast"])
 
     return result
@@ -70,10 +95,8 @@ if __name__ == "__main__":
     import json
     import os.path
 
-    file_example = (
-        "/home/juand/Documents/vscode/proyecto/etl-design/"
-        "/typechecking/backend/static/acme__users__sample1.xlsx"
-    )
+    file_example = "../../typechecking/backend/static/acme__users__sample1.xlsx"
+
     with open(file_example, "rb") as file:
         file_bytes = file.read()
 
