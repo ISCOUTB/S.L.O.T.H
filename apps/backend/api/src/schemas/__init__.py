31010bea8e4eba02bd78cd7b391c7f0df2d1deff
JuanD <dosquisd123@gmail.com>
2025-09-07 15:32:28 -0500

    chore(api): update __init__.py's for autocompletion

---
+++ apps/backend/api/src/schemas/__init__.py
@@ -0,0 +1,16 @@
+from src.schemas import api
+from src.schemas import controllers
+from src.schemas import messaging
+from src.schemas import models
+from src.schemas import token
+from src.schemas import users
+
+
+__all__ = [
+    "api",
+    "controllers",
+    "messaging",
+    "models",
+    "token",
+    "users",
+]
