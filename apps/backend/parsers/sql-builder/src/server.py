4538466115d8dac21c2e870bbcf412c6f9f4b0da
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2025-09-10 16:38:41 -0500

    chore: integrating sql-builder more with moonrepo

--- apps/backend/parsers/sql-builder/src/server.py
+++ apps/backend/parsers/sql-builder/src/server.py
@@ -1,21 +1,20 @@
 import asyncio
-import logging
+import signal
 
 import grpc
-from clients import sql_builder_pb2, sql_builder_pb2_grpc
+from grpc._typing import Any  # type: ignore
 
+from clients import sql_builder_pb2, sql_builder_pb2_grpc
 from core.config import settings
 from handlers.sql_builder import sql_builder_handler
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("SQLBuilderServer")
+from utils import logger
 
 
 class SQLBuilderServicer(sql_builder_pb2_grpc.SQLBuilderServicer):
     def BuildSQL(
         self,
         request: sql_builder_pb2.BuildSQLRequest,
-        _: grpc.aio.ServicerContext,
+        context: grpc.aio.ServicerContext[Any, Any],
     ) -> sql_builder_pb2.BuildSQLResponse:
         logger.info("Received SQL building request")
         return sql_builder_handler(request)
@@ -23,16 +22,32 @@ class SQLBuilderServicer(sql_builder_pb2_grpc.SQLBuilderServicer):
 
 async def serve() -> None:
     server = grpc.aio.server()
-    sql_builder_pb2_grpc.add_SQLBuilderServicer_to_server(SQLBuilderServicer(), server)
-    server.add_insecure_port(settings.SQL_BUILDER_CHANNEL)
-    await server.start()
-    logger.info(
-        (
-            f"SQL Builder server started on {settings.SQL_BUILDER_CHANNEL}"
-            f" -- DEBUG: {settings.SQL_BUILDER_DEBUG}"
-        )
+
+    sql_builder_pb2_grpc.add_SQLBuilderServicer_to_server(  # type: ignore
+        SQLBuilderServicer(), server
     )
-    await server.wait_for_termination()
+
+    server.add_insecure_port(settings.SQL_BUILDER_CHANNEL)
+
+    await server.start()
+
+    logger.info(f"running on {settings.SQL_BUILDER_CHANNEL}")
+    logger.info(f"debug: {settings.SQL_BUILDER_DEBUG}")
+
+    stop_event = asyncio.Event()
+
+    def _signal_handler() -> None:
+        logger.info("stop requested")
+        stop_event.set()
+
+    loop = asyncio.get_running_loop()
+    loop.add_signal_handler(signal.SIGINT, _signal_handler)
+    loop.add_signal_handler(signal.SIGTERM, _signal_handler)
+
+    await stop_event.wait()
+    await server.stop(grace=5)
+
+    logger.info("stopped")
 
 
 if __name__ == "__main__":
