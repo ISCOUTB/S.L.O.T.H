0803a38498d7d33cb7099d4fa0b34f0232ec97d4
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2025-09-10 15:17:41 -0500

    chore: remove dotenv dependency each service will have his own .env file inside the directory

--- apps/backend/parsers/formula-parser/src/server.js
+++ apps/backend/parsers/formula-parser/src/server.js
@@ -1,32 +1,34 @@
-var grpc = require('@grpc/grpc-js');
-const { settings } = require('./core/config');
-var services = require('./clients/formula_parser_grpc_pb');
-var { parseFormulaHandler } = require('./handlers/formulaParserHandler');
-
+const grpc = require("@grpc/grpc-js");
+const services = require("./clients/formula_parser_grpc_pb");
+const { settings } = require("./core/config");
+const { parseFormulaHandler } = require("./handlers/formulaParserHandler");
 
 function parseFormula(call, callback) {
     callback(null, parseFormulaHandler(call.request.getFormula()));
 }
 
-
 function getServer() {
-    var server = new grpc.Server();
+    const server = new grpc.Server();
     server.addService(services.FormulaParserService, {
-        parseFormula: parseFormula
+        parseFormula,
     });
     return server;
 }
 
-
 if (require.main === module) {
-    var routeServer = getServer();
+    const routeServer = getServer();
     const { host, port, debug } = settings;
-
-    routeServer.bindAsync(`${host}:${port}`, grpc.ServerCredentials.createInsecure(), (err, port) => {
-        if (err) {
-            console.error('Failed to bind server:', err);
-            return;
-        }
-        console.log(`Formula Parser Server on ${host}:${port} -- DEBUG: ${debug}`);
-    });
+    routeServer.bindAsync(
+        `${host}:${port}`,
+        grpc.ServerCredentials.createInsecure(),
+        (err, port) => {
+            if (err) {
+                console.error("Failed to bind server:", err);
+                return;
+            }
+            console.warn(
+                `Formula Parser Server on ${host}:${port} -- DEBUG: ${debug}`,
+            );
+        },
+    );
 }
