66c0e44103fdd69cf02c2054bff41bc110f9e66b
JuanD <dosquisd123@gmail.com>
2025-09-16 15:36:06 -0500

    feat(proto-utils-py): create a base_client in order to connect and use database service

--- packages/proto-utils-py/src/proto_utils/database/__init__.py
+++ packages/proto-utils-py/src/proto_utils/database/__init__.py
@@ -4,7 +4,11 @@ from proto_utils.database import (
     redis_serde,
     mongo_serde,
     database_serde,
+    base_client,
 )
+
+from proto_utils.database.base_client import DatabaseClient
+
 from proto_utils.database.mongo_serde import MongoSerde
 from proto_utils.database.redis_serde import RedisSerde
 from proto_utils.database.database_serde import DatabaseSerde
@@ -17,6 +21,8 @@ __all__ = [
     "redis_serde",
     "mongo_serde",
     "database_serde",
+    "base_client",
+    "DatabaseClient",
     "MongoSerde",
     "RedisSerde",
     "DatabaseSerde",
