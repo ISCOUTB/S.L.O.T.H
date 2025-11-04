7322519ebdb8fcac39abcff20474e7fc0077b0f0
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2025-11-04 15:27:04 -0500

    fix: docker stack doesn't load the .env variables by default
    - at least, it should when the variables are in the same directory, but when running in moon ci the .yml are nested in directories
    
    - So, the scaler now uses NODE_ENV variable to load variables only when is in 'development', when is in 'staging' and 'production' it will *ONLY* read variables, not load it.
    
    - with that in mind, using a intermediate script seems to be the easiest solution for loading (and exporting) the variables.

--- packages/autoscaler/src/config/env.py
+++ packages/autoscaler/src/config/env.py
@@ -1,10 +1,19 @@
+import os
+from typing import Literal
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from src.utils.rootdir import ROOTDIR
 
+if os.getenv("NODE_ENV", "development") == "development":
+    from dotenv import load_dotenv
+
+    load_dotenv(ROOTDIR / ".env")
+
 
 class EnvSettings(BaseSettings):
     DEBUG: bool = False
+    NODE_ENV: Literal["development", "staging", "production"] = "development"
 
     PROMETHEUS_URL: str = "http://prometheus:9090/"
 
@@ -20,7 +29,6 @@ class EnvSettings(BaseSettings):
         return self.CHECK_INTERVAL * self.METRIC_WINDOW_MULTIPLIER
 
     model_config = SettingsConfigDict(
-        env_file=(ROOTDIR / ".env").absolute(),
         env_file_encoding="utf-8",
         case_sensitive=False,
     )
