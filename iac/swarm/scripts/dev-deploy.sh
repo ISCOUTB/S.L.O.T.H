24b396aafa0f1e01a2db21cc6460d054c7d1864f
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2025-11-14 15:16:09 -0500

    feat: update formula-parser and environment configurations, enhance ESLint setup

--- iac/swarm/scripts/dev-deploy.sh
+++ iac/swarm/scripts/dev-deploy.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
-set -e -a
+set -e
 
-source swarm/development/.env
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+ENV_FILE="swarm/development/.env"
+STACK_FILE="swarm/development/docker-stack.yml"
+
+if [ ! -f "$ENV_FILE" ]; then
+    echo -e "${RED}[ ERROR ] file does not exists: $ENV_FILE${NC}"
+    exit 1
+fi
+
+if [ ! -f "$STACK_FILE" ]; then
+    echo -e "${RED}[ ERROR ] file does not exists: $STACK_FILE${NC}"
+    exit 1
+fi
+
+echo -e "${GREEN}[ INFO ] loading .env vars from $ENV_FILE...${NC}"
+set -a
+source "$ENV_FILE"
 set +a
 
-docker stack deploy -c swarm/development/docker-stack.yml "$GLOBAL_STACK_NAME" --detach=true
+if [ -z "$GLOBAL_STACK_NAME" ]; then
+    echo -e "${RED}[ ERROR] 'GLOBAL_STACK_NAME' is required $ENV_FILE${NC}"
+    exit 1
+fi
+
+echo -e "${GREEN}[ INFO ] deploying stack: $GLOBAL_STACK_NAME${NC}"
+docker stack deploy -c "$STACK_FILE" "$GLOBAL_STACK_NAME" --detach=true
+
+echo -e "${GREEN}[ SUCCESS ] done${NC}"
